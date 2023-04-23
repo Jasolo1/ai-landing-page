51b5399dee7458cc1ae7c6803faa263f1a9b356c
jasolo <jasolo@ttu.ee>
2023-04-23 19:19:43 +0300

    check commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="mt-10 text-center">  
-        <h1 className="p-5 text-2xl">Генератор лендингов на основе AI</h1>
+        <h1 className="p-5 text-2xl">Генератор лендингов на основе AI (jan(ssh)) </h1>
     </main>
   )
 }
